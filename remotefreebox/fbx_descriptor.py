c00bda867833e7a571ec7af3c718774e83b0bc42
Maxime Chéramy <maxime.cheramy@gmail.com>
2016-01-03 17:39:06 +0100

    Update list of commands to match documentation.

--- remotefreebox/fbx_descriptor.py
+++ remotefreebox/fbx_descriptor.py
@@ -2,58 +2,58 @@ from .rudp_hid_client import foils_hid_device_descriptor
 
 
 unicode_report_descriptor = bytes([
-    0x05, 0x01,         #   Usage Page (Desktop),
-    0x09, 0x06,         #   Usage (Keyboard),
+    0x05, 0x01,              # Usage Page (Desktop),
+    0x09, 0x06,              # Usage (Keyboard),
 
-    0xA1, 0x01,         #   Collection (Application),
-    0x85, 0x01,         #       Report ID (1),
-    0x05, 0x10,         #       Usage Page (Unicode),
-    0x08,               #       Usage (00h),
-    0x95, 0x01,         #       Report Count (1),
-    0x75, 0x20,         #       Report Size (32),
-    0x14,               #       Logical Minimum (0),
-    0x27, 0xFF, 0xFF, 0xFF,  #       Logical Maximum (2**24-1),
-    0x81, 0x62,         #       Input (Variable, No pref state, No Null Pos),
-    0xC0,               #   End Collection
+    0xA1, 0x01,              # Collection (Application),
+    0x85, 0x01,              # . Report ID (1),
+    0x05, 0x10,              # . Usage Page (Unicode),
+    0x08,                    # . Usage (00h),
+    0x95, 0x01,              # . Report Count (1),
+    0x75, 0x20,              # . Report Size (32),
+    0x14,                    # . Logical Minimum (0),
+    0x27, 0xFF, 0xFF, 0xFF,  # . Logical Maximum (2**24-1),
+    0x81, 0x62,              # . Input (Variable, No pref state, No Null Pos),
+    0xC0,                    # End Collection
 
-    0xA1, 0x01,         #   Collection (Application),
-    0x85, 0x02,         #       Report ID (2),
-    0x95, 0x01,         #       Report Count (1),
-    0x75, 0x08,         #       Report Size (8),
-    0x15, 0x00,         #       Logical Minimum (0),
-    0x26, 0xFF, 0x00,   #       Logical Maximum (255),
-    0x05, 0x07,         #       Usage Page (Keyboard),
-    0x19, 0x00,         #       Usage Minimum (None),
-    0x2A, 0xFF, 0x00,   #       Usage Maximum (FFh),
-    0x80,               #       Input,
-    0xC0,               #   End Collection
+    0xA1, 0x01,              # Collection (Application),
+    0x85, 0x02,              # . Report ID (2),
+    0x95, 0x01,              # . Report Count (1),
+    0x75, 0x08,              # . Report Size (8),
+    0x15, 0x00,              # . Logical Minimum (0),
+    0x26, 0xFF, 0x00,        # . Logical Maximum (255),
+    0x05, 0x07,              # . Usage Page (Keyboard),
+    0x19, 0x00,              # . Usage Minimum (None),
+    0x2A, 0xFF, 0x00,        # . Usage Maximum (FFh),
+    0x80,                    # . Input,
+    0xC0,                    # End Collection
 
-    0x05, 0x0C,         #  Usage Page (Consumer),
-    0x09, 0x01,         #  Usage (Consumer Control),
-    0xA1, 0x01,         #  Collection (Application),
-    0x85, 0x03,         #   Report ID (3),
-    0x95, 0x01,         #   Report Count (1),
-    0x75, 0x10,         #   Report Size (16),
-    0x19, 0x00,         #   Usage Minimum (Consumer Control),
-    0x2A, 0x8C, 0x02,   #   Usage Maximum (AC Send),
-    0x15, 0x00,         #   Logical Minimum (0),
-    0x26, 0x8C, 0x02,   #   Logical Maximum (652),
-    0x80,               #   Input,
-    0xC0,               #  End Collection,
+    0x05, 0x0C,              # Usage Page (Consumer),
+    0x09, 0x01,              # Usage (Consumer Control),
+    0xA1, 0x01,              # Collection (Application),
+    0x85, 0x03,              # . Report ID (3),
+    0x95, 0x01,              # . Report Count (1),
+    0x75, 0x10,              # . Report Size (16),
+    0x19, 0x00,              # . Usage Minimum (Consumer Control),
+    0x2A, 0x8C, 0x02,        # . Usage Maximum (AC Send),
+    0x15, 0x00,              # . Logical Minimum (0),
+    0x26, 0x8C, 0x02,        # . Logical Maximum (652),
+    0x80,                    # . Input,
+    0xC0,                    # End Collection,
 
-    0x05, 0x01,         #  Usage Page (Desktop),
-    0x0a, 0x80, 0x00,   #  Usage (System Control),
-    0xA1, 0x01,         #  Collection (Application),
-    0x85, 0x04,         #   Report ID (4),
-    0x75, 0x01,         #   Report Size (1),
-    0x95, 0x04,         #   Report Count (4),
-    0x1a, 0x81, 0x00,   #   Usage Minimum (System Power Down),
-    0x2a, 0x84, 0x00,   #   Usage Maximum (System Context menu),
-    0x81, 0x02,         #   Input (Variable),
-    0x75, 0x01,         #   Report Size (1),
-    0x95, 0x04,         #   Report Count (4),
-    0x81, 0x01,         #   Input (Constant),
-    0xC0,               #  End Collection,
+    0x05, 0x01,              # Usage Page (Desktop),
+    0x0a, 0x80, 0x00,        # Usage (System Control),
+    0xA1, 0x01,              # Collection (Application),
+    0x85, 0x04,              # . Report ID (4),
+    0x75, 0x01,              # . Report Size (1),
+    0x95, 0x04,              # . Report Count (4),
+    0x1a, 0x81, 0x00,        # . Usage Minimum (System Power Down),
+    0x2a, 0x84, 0x00,        # . Usage Maximum (System Context menu),
+    0x81, 0x02,              # . Input (Variable),
+    0x75, 0x01,              # . Report Size (1),
+    0x95, 0x04,              # . Report Count (4),
+    0x81, 0x01,              # . Input (Constant),
+    0xC0,                    # End Collection,
 ])
 
 fbx_foils_hid_device_descriptor = foils_hid_device_descriptor(
@@ -81,12 +81,27 @@ HID_KEYBOARD_UPARROW = 0x52
 HID_KEYBOARD_HOME = 0x4A
 HID_KEYBOARD_POWER = 0x66
 HID_KEYBOARD_F1 = 0x3A
+HID_KEYBOARD_KEYPAD_1 = 0x59
+HID_KEYBOARD_KEYPAD_2 = 0x5A
+HID_KEYBOARD_KEYPAD_3 = 0x5B
+HID_KEYBOARD_KEYPAD_4 = 0x5C
+HID_KEYBOARD_KEYPAD_5 = 0x5D
+HID_KEYBOARD_KEYPAD_6 = 0x5E
+HID_KEYBOARD_KEYPAD_7 = 0x5F
+HID_KEYBOARD_KEYPAD_8 = 0x60
+HID_KEYBOARD_KEYPAD_9 = 0x61
+HID_KEYBOARD_KEYPAD_0 = 0x62
 
+HID_CONSUMER_POWER = 0x30
+HID_CONSUMER_MENU = 0x40
+HID_CONSUMER_DATA_ON_SCREEN = 0x60
+HID_CONSUMER_VCR_TV = 0x63
 HID_CONSUMER_SUB_CHANNEL_INCREMENT = 0x171
 HID_CONSUMER_ALTERNATE_AUDIO_INCREMENT = 0x173
 HID_CONSUMER_ALTERNATE_SUBTITLE_INCREMENT = 0x175
 HID_CONSUMER_CHANNEL_INCREMENT = 0x9c
 HID_CONSUMER_CHANNEL_DECREMENT = 0x9d
+HID_CONSUMER_PLAY_PAUSE = 0xcd
 HID_CONSUMER_PLAY = 0xb0
 HID_CONSUMER_PAUSE = 0xb1
 HID_CONSUMER_RECORD = 0xb2
@@ -101,6 +116,11 @@ HID_CONSUMER_VOLUME_INCREMENT = 0xe9
 HID_CONSUMER_VOLUME_DECREMENT = 0xea
 HID_CONSUMER_RANDOM_PLAY = 0xb9
 
+HID_CONSUMER_BROWSER_BACK = 0x224
+HID_CONSUMER_BROWSER_FORWARD = 0x225
+HID_CONSUMER_BROWSER_REFRESH = 0x227
+HID_CONSUMER_BROWSER_STOP = 0x226
+
 HID_CONSUMER_AC_ZOOM_IN = 0x22d
 HID_CONSUMER_AC_ZOOM_OUT = 0x22e
 
@@ -112,41 +132,68 @@ HID_CONSUMER_AL_TASK_MANAGER = 0x18f
 HID_CONSUMER_AL_INTERNET_BROWSER = 0x196
 HID_CONSUMER_AL_AUDIO_BROWSER = 0x1b7
 
+HID_DESKTOP_MENU = 0x86
+
+# Documentation available here:
+# http://dev.freebox.fr/sdk/freebox_player_1.1.4_codes.html
 fbx_target_command_codes = {
-    "Enter": (TARGET_KEYBOARD, HID_KEYBOARD_ENTER),
-    "Tab": (TARGET_KEYBOARD, HID_KEYBOARD_TAB),
-    "Power/kbd": (TARGET_KEYBOARD, HID_KEYBOARD_POWER),
-    "Mute": (TARGET_CONSUMER, HID_CONSUMER_MUTE),
-    "Backspace": (TARGET_KEYBOARD, HID_KEYBOARD_BACKSPACE),
+    # Remote control emulation
+    "Power": (TARGET_CONSUMER, HID_CONSUMER_POWER),
+    "AV": (TARGET_CONSUMER, HID_CONSUMER_VCR_TV),
+    "0": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_0),
+    "1": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_1),
+    "2": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_2),
+    "3": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_3),
+    "4": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_4),
+    "5": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_5),
+    "6": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_6),
+    "7": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_7),
+    "8": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_8),
+    "9": (TARGET_KEYBOARD, HID_KEYBOARD_KEYPAD_9),
     "Up": (TARGET_KEYBOARD, HID_KEYBOARD_UPARROW),
     "Down": (TARGET_KEYBOARD, HID_KEYBOARD_DOWNARROW),
     "Left": (TARGET_KEYBOARD, HID_KEYBOARD_LEFTARROW),
     "Right": (TARGET_KEYBOARD, HID_KEYBOARD_RIGHTARROW),
-    "Task Manager": (TARGET_CONSUMER, HID_CONSUMER_AL_TASK_MANAGER),
-    "AC Search": (TARGET_CONSUMER, HID_CONSUMER_AC_SEARCH),
-    "AC Exit": (TARGET_CONSUMER, HID_CONSUMER_AC_EXIT),
-    "Context Menu": (TARGET_DESKTOP, DC_SYSTEM_CONTEXT_MENU),
-    "AC Properties": (TARGET_CONSUMER, HID_CONSUMER_AC_PROPERTIES),
-    "Eject": (TARGET_CONSUMER, HID_CONSUMER_EJECT),
-    "AL Internet Browser": (TARGET_CONSUMER, HID_CONSUMER_AL_INTERNET_BROWSER),
-    "Zoom +": (TARGET_CONSUMER, HID_CONSUMER_AC_ZOOM_IN),
+    "Enter": (TARGET_KEYBOARD, HID_KEYBOARD_ENTER),
+    "Back": (TARGET_CONSUMER, HID_CONSUMER_AC_EXIT),
+    "Search": (TARGET_CONSUMER, HID_CONSUMER_AC_SEARCH),
+    "Menu": (TARGET_CONSUMER, HID_CONSUMER_MENU),
+    "Info": (TARGET_CONSUMER, HID_CONSUMER_AC_PROPERTIES),
+    "Free": (TARGET_CONSUMER, HID_CONSUMER_AL_TASK_MANAGER),
     "Vol-": (TARGET_CONSUMER, HID_CONSUMER_VOLUME_DECREMENT),
     "Vol+": (TARGET_CONSUMER, HID_CONSUMER_VOLUME_INCREMENT),
-    "System Sleep": (TARGET_DESKTOP, DC_SYSTEM_POWER_SLEEP),
-    "System Wakeup": (TARGET_DESKTOP, DC_SYSTEM_POWER_WAKEUP),
-    "Stop": (TARGET_CONSUMER, HID_CONSUMER_STOP),
-    "Rewind": (TARGET_CONSUMER, HID_CONSUMER_REWIND),
-    "Play": (TARGET_CONSUMER, HID_CONSUMER_PLAY),
-    "FastForward": (TARGET_CONSUMER, HID_CONSUMER_FAST_FORWARD),
+    "Mute": (TARGET_CONSUMER, HID_CONSUMER_MUTE),
     "Record": (TARGET_CONSUMER, HID_CONSUMER_RECORD),
+    "Chan+": (TARGET_CONSUMER, HID_CONSUMER_CHANNEL_INCREMENT),
+    "Chan-": (TARGET_CONSUMER, HID_CONSUMER_CHANNEL_DECREMENT),
+    "Rewind": (TARGET_CONSUMER, HID_CONSUMER_REWIND),
+    "Play/Pause": (TARGET_CONSUMER, HID_CONSUMER_PLAY_PAUSE),
+    "Fast Forward": (TARGET_CONSUMER, HID_CONSUMER_FAST_FORWARD),
+
+    # Additional keys
+    "Backspace": (TARGET_KEYBOARD, HID_KEYBOARD_BACKSPACE),
+    "Stream debug": (TARGET_CONSUMER, HID_CONSUMER_DATA_ON_SCREEN),
+    "Stop": (TARGET_CONSUMER, HID_CONSUMER_STOP),
+    "Play": (TARGET_CONSUMER, HID_CONSUMER_PLAY),
     "Random Play": (TARGET_CONSUMER, HID_CONSUMER_RANDOM_PLAY),
     "Previous track": (TARGET_CONSUMER, HID_CONSUMER_SCAN_PREVIOUS_TRACK),
     "Next track": (TARGET_CONSUMER, HID_CONSUMER_SCAN_NEXT_TRACK),
+    "Zoom+": (TARGET_CONSUMER, HID_CONSUMER_AC_ZOOM_IN),
+    "Zoom-": (TARGET_CONSUMER, HID_CONSUMER_AC_ZOOM_OUT),
+    "Browser Back": (TARGET_CONSUMER, HID_CONSUMER_BROWSER_BACK),
+    "Browser Forward": (TARGET_CONSUMER, HID_CONSUMER_BROWSER_FORWARD),
+    "Browser Refresh": (TARGET_CONSUMER, HID_CONSUMER_BROWSER_REFRESH),
+    "Browser Stop": (TARGET_CONSUMER, HID_CONSUMER_BROWSER_STOP),
     "Video Track": (TARGET_CONSUMER, HID_CONSUMER_SUB_CHANNEL_INCREMENT),
     "Audio Track": (TARGET_CONSUMER, HID_CONSUMER_ALTERNATE_AUDIO_INCREMENT),
     "Subtitle Track": (TARGET_CONSUMER, HID_CONSUMER_ALTERNATE_SUBTITLE_INCREMENT),
-    "Chan+": (TARGET_CONSUMER, HID_CONSUMER_CHANNEL_INCREMENT),
-    "Chan-": (TARGET_CONSUMER, HID_CONSUMER_CHANNEL_DECREMENT)
+    "System Sleep": (TARGET_DESKTOP, DC_SYSTEM_POWER_SLEEP),
+    "System Wakeup": (TARGET_DESKTOP, DC_SYSTEM_POWER_WAKEUP),
+    "Eject": (TARGET_CONSUMER, HID_CONSUMER_EJECT),
+    "Tab": (TARGET_KEYBOARD, HID_KEYBOARD_TAB),
+    "Power/kbd": (TARGET_KEYBOARD, HID_KEYBOARD_POWER),
+    "Context Menu": (TARGET_DESKTOP, DC_SYSTEM_CONTEXT_MENU),
+    "AL Internet Browser": (TARGET_CONSUMER, HID_CONSUMER_AL_INTERNET_BROWSER)
 }
 
 
